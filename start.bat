3a25fbc3afafa4483c67e3414d19ba2b60e4e319
Lincolncowper <lincolncowper@outlook.com>
2024-11-21 01:43:18 +0700

    Hendra Coders

--- start.bat
+++ start.bat
@@ -1,19 +1,18 @@
 @echo off
-del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows 11 Server by HenCoders" > out.txt 2>&1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator HEN2024 /add >nul
-net localgroup administrators administrator /add >nul
-net user administrator /active:yes >nul
-net user installer /delete
+net user Administrator /active:yes
+net user Administrator HEN2024 /add
+net localgroup administrators Administrator /add
+net accounts /minpwlen:1 /maxpwage:unlimited /minpwage:1 /passwordchg:yes
+
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\Installer /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+
 echo Successfully Installed, If the RDP is Dead, Please Rebuild Again!
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get a tunnel, make sure ngrok_auth_token is correct in Settings> Secrets> Repository Secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels"
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get a tunnel, make sure ngrok_auth_token is correct in Settings> Secrets> Repository Secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
 echo Password: HEN2024
 echo Please log in to your RDP!
