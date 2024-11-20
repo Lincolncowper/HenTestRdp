73d03a31035b34d32d5103456b62b76b46a97799
Lincolncowper <lincolncowper@outlook.com>
2024-11-21 05:11:09 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,7 @@
 @echo off
-del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server by HenCoders" > out.txt 2>&1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat > out.txt 2>&1
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" >nul 2>&1
+net config server /srvcomment:"Windows Server by HenCoders" >nul 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F >nul 2>&1
 net user administrator HenCoders13 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
@@ -12,15 +11,13 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-
-:: Menonaktifkan "Test Mode" di kanan bawah
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v ShowUserInfoAtLockScreen /t REG_DWORD /d 0 /f > nul
-bcdedit -set testsigning off
-
-echo Instalasi Berhasil, Jika RDP Tidak Berfungsi, Silakan Bangun Kembali!
+bcdedit -set TESTSIGNING OFF >nul 2>&1
+echo Instalasi selesai! Jika RDP mati, silakan bangun ulang.
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak dapat mendapatkan terowongan, pastikan ngrok_auth_token sudah benar di Pengaturan > Secrets > Repository Secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+tasklist | find /i "ngrok.exe" >nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Gagal mendapatkan tunnel. Pastikan NGROK_AUTH_TOKEN benar di Settings > Secrets > Repository Secret. Atau mungkin VM sebelumnya masih aktif: https://dashboard.ngrok.com/status/tunnels"
 echo Username: administrator
 echo Password: HenCoders13
 echo Silakan login ke RDP Anda!
-ping -n 10 127.0.0.1 >nul
+echo Komputer akan restart dalam 10 detik...
+timeout /t 10 /nobreak >nul
+shutdown /r /t 0
