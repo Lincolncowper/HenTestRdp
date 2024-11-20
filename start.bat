e9342e1784561ebc3697320ec6c021799f28e8d9
Lincolncowper <lincolncowper@outlook.com>
2024-11-21 01:31:12 +0700

    Hendra Coders

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows 11 Server by HenCoders" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator W2016 /add >nul
+net user administrator HEN2024 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -15,6 +15,6 @@ echo Successfully Installed, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get a tunnel, make sure ngrok_auth_token is correct in Settings> Secrets> Repository Secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels"
 echo Username: administrator
-echo Password: W2016
+echo Password: HEN2024
 echo Please log in to your RDP!
 ping -n 10 127.0.0.1 >nul
